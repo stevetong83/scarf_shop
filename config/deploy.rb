01ce9cbb1af46bb02026e99213ef18d5025be956
Stephen Tong <stephentong@Stephens-MacBook-Pro-2.local>
2015-01-08 12:42:00 -0700

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,20 +37,14 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-    end
-    invoke 'unicorn:reload'
+
   end
 
   after :publishing, :restart
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+    invoke 'unicorn:restart'
+    invoke 'unicorn:reload'
   end
 
 end
