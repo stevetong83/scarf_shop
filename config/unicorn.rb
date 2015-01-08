293347507e92a621b8f6114b9fb48cb87e846d05
Stephen Tong <stephentong@Stephens-MacBook-Pro-2.local>
2015-01-08 12:50:36 -0700

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/www/pretty_as_a_picture_scarves/current"
+root = "~/www/pretty_as_a_picture_scarves/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.pretty_as_a_picture_scarves.sock"
-worker_processes 1
+worker_processes 2
 timeout 30
